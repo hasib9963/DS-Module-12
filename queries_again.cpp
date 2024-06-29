6e1dffbb739a934236c2dd21f0bccbbf6cf7db72
Hasib Mohammad Mahede Hasan <hasib9963@gmail.com>
2024-06-30 00:27:06 +0600

    insert value into doubly Linked List at given index

---
+++ queries_again.cpp
@@ -0,0 +1,130 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public:
+    int val;
+    Node* next;
+    Node* prev;
+    Node(int val)
+    {
+        this->val=val;
+        this->next=NULL;
+        this->prev=NULL;
+    }
+};
+void insert_at_position(Node* head, int pos, int val)
+{
+    Node* newNode = new Node(val);
+    Node* tmp = head;
+    for(int i=1; i<=pos-1;i++)
+    {
+        tmp = tmp->next;
+    }
+    newNode->next = tmp->next;
+    tmp->next = newNode; 
+    newNode->next->prev = newNode; 
+    newNode->prev = tmp; 
+}
+void print_normal(Node* head)
+{
+    cout<<"L ->" << " ";
+    Node* tmp = head;
+    while(tmp!=NULL)
+    {
+        cout<<tmp->val<<" ";
+        tmp = tmp->next;
+    }
+    //cout<<endl;
+}
+void print_reverse(Node* tail)
+{
+    cout<<"R ->" << " ";
+    Node* tmp = tail;
+    while(tmp!=NULL)
+    {
+        cout<<tmp->val<<" ";
+        tmp = tmp->prev;
+    }
+    cout<<endl;
+}
+int size(Node* head)
+{
+    Node* tmp = head;
+    int cnt = 0;
+    while(tmp != NULL)
+    {
+        cnt++;
+        tmp = tmp->next;
+    }
+    return cnt;
+}
+void insert_head(Node* &head, Node* &tail, int val)
+{
+    Node* newNode = new Node(val);
+    if(head == NULL)
+    {
+        Node* newNode = new Node(val);
+        if(head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        newNode->next = head;
+        head->prev = newNode;
+        head = newNode;
+    }
+    newNode->next = head;
+    head->prev = newNode;
+
+    head = newNode;
+}
+void insert_tail(Node* &head, Node* &tail, int val)
+{
+    Node* newNode = new Node(val);
+    if(tail == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    newNode->prev = tail;
+
+    tail = tail->next;
+}
+int main()
+{
+    Node* head = NULL;
+    Node* tail = NULL;
+    int q;
+    cin>>q;
+    while(q--)
+    {
+    int pos, val;
+    cin>>pos>>val;
+    if(pos == 0)
+    {
+        insert_head(head, tail, val);
+    }
+
+    else if(pos == size(head))
+    {
+        insert_tail(head,tail,val);
+    }
+    else if(pos>=size(head))
+    {
+        cout<<"Invalid"<<endl;
+        continue;
+    }
+    else
+    {
+        insert_at_position(head, pos, val);
+    }
+    print_normal(head);
+    cout<<endl;
+    print_reverse(tail);
+    }
+    return 0;
+}
